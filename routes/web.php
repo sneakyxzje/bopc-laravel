014f767fc7cad85713e1786de0d460b8968d225d
zenitsugocrazy <tuandattobi@gmail.com>
2026-03-25 13:44:43 +0700

    feat(route): add route for customer and admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\Admin\OrderController as AdminOrderController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'index']);
+
+// Order routes, dành cho customer
+Route::prefix('order')->name('order.')->group(function () {
+    Route::get('/checkout', [OrderController::class, 'index'])->name('checkout');
+    Route::post('/checkout', [OrderController::class, 'store'])->name('store');
+    Route::get('/checkout/success', [OrderController::class, 'vnpayReturn'])->name('vnpay_return');
+});
+
+Route::prefix('admin/orders')->name('admin.orders.')->group(function () {
+    Route::get('/', [AdminOrderController::class, 'index'])->name('index');
+    Route::get('/{id}', [AdminOrderController::class, 'show'])->name('show');
+    Route::post('/{id}/update-status', [AdminOrderController::class, 'updateStatus'])->name('update_status');
+});
