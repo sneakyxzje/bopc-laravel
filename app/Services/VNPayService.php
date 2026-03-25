2a67ef39f18911d4877c582f388238dcb0135729
zenitsugocrazy <tuandattobi@gmail.com>
2026-03-25 13:39:57 +0700

    feat(order): add vnpay service for payment

---
+++ app/Services/VNPayService.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Services;
+
+class VNPayService
+{
+    public function createPayment($order)
+    {
+        $vnp_TmnCode = env('vnp_TmnCode');
+        $vnp_HashSecret = env('vnp_HashSecret');
+
+        $vnp_Url = env('vnp_Url');
+        $vnp_ReturnUrl = env('vnp_ReturnUrl');
+
+        $vnp_TxnRef = $order->id;
+        $vnp_OrderInfo = "Payment for order #" . $order->id;
+        $vnp_Amount = $order->total_price * 100;
+        $vnp_Locale = 'vn';
+        $vnp_IpAddr = request()->ip();
+
+        $inputData = [
+            "vnp_Version" => "2.1.0",
+            "vnp_TmnCode" => $vnp_TmnCode,
+            "vnp_Amount" => $vnp_Amount,
+            "vnp_Command" => "pay",
+            "vnp_CreateDate" => date('YmdHis'),
+            "vnp_CurrCode" => "VND",
+            "vnp_IpAddr" => $vnp_IpAddr,
+            "vnp_Locale" => $vnp_Locale,
+            "vnp_OrderInfo" => $vnp_OrderInfo,
+            "vnp_OrderType" => 'billpayment',
+            "vnp_ReturnUrl" => $vnp_ReturnUrl,
+            "vnp_TxnRef" => $vnp_TxnRef,
+        ];
+
+        ksort($inputData);
+        $query = "";
+        $i = 0;
+        $hashdata = "";
+        foreach ($inputData as $key => $value) {
+            if ($i == 1) {
+                $hashdata .= '&' . urlencode($key) . "=" . urlencode($value);
+            } else {
+                $hashdata .= urlencode($key) . "=" . urlencode($value);
+                $i = 1;
+            }
+            $query .= urlencode($key) . "=" . urlencode($value) . '&';
+        }
+
+        $vnp_Url = $vnp_Url . "?" . $query;
+        if (isset($vnp_HashSecret)) {
+            $vnpSecureHash = hash_hmac('sha512', $hashdata, $vnp_HashSecret);
+            $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
+        }
+
+        return $vnp_Url;
+    }
+}
