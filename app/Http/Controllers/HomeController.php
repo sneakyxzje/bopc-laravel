9939ef6c67e7626e77b14532b9b0e579db34b4a9
zenitsugocrazy <tuandattobi@gmail.com>
2026-03-27 04:32:48 +0700

    feat(product): show all product in home page, support paginate

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
-use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\ProductVariant;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $products = Product::with(['primaryImage', 'category'])
+            ->addSelect([
+                'final_min_price' => ProductVariant::selectRaw('MIN(CASE WHEN sale_price IS NOT NULL AND sale_price > 0 THEN sale_price ELSE price END)')
+                    ->whereColumn('product_id', 'products.id')
+            ])
+            ->addSelect([
+                'final_max_price' => ProductVariant::selectRaw('MAX(CASE WHEN sale_price IS NOT NULL AND sale_price > 0 THEN sale_price ELSE price END)')
+                    ->whereColumn('product_id', 'products.id')
+            ])
+            ->withMin('variants', 'price')
+            ->where('is_active', true)
+            ->paginate(12);
+
+        return view('home', compact('products'));
     }
 }
